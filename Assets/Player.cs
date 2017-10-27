0c57b1d4daa6f4176a79a1a147b1391e0b26db14
VIRUS\heref <hereforlife2003@gmail.com>
2017-10-27 15:36:28 -0700

    Added Code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -12,6 +12,9 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+
+    }
 }
